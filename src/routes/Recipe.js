ddbb0175fdcd2a99a96213108d3c5a1117638915
kenjixcz <kenji.k23@mhs.istts.ac.id>
2025-06-20 00:41:19 +0700

    recipe ketambahan multer tapi ada yang kurang

--- src/routes/Recipe.js
+++ src/routes/Recipe.js
@@ -7,15 +7,23 @@ const {
   deleteRecipe,
   getRecipeByIngredients,
   getRecipeByNutrients,
+  getRecipebyUser,
 } = require("../controllers/recipeController");
 const verifyToken = require("../middlewares/authMiddleware");
+const { uploadSingleImage } = require("../utils/multer/multer");
+const resizeImage = require("../middlewares/resizeImage");
 const router = express.Router();
 
 router.get("/getAllRecipes", getAllRecipe);
 router.get("/getDetailRecipe/:id", getDetailRecipe);
+router.get("/getRecipeByUser", [verifyToken], getRecipebyUser);
 router.get("/getRecipeByIngredients", getRecipeByIngredients);
 router.get("/getRecipeByNutrients", getRecipeByNutrients);
-router.post("/insertRecipe", [verifyToken], insertRecipe);
+router.post(
+  "/insertRecipe",
+  [verifyToken, uploadSingleImage("foodImage")],
+  insertRecipe
+);
 router.put("/updateRecipe/:id", [verifyToken], updateRecipe);
 router.delete("/deleteRecipe/:id", [verifyToken], deleteRecipe);
 module.exports = router;
